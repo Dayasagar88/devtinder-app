f44ea764f66b3e93b291a0dba5281b96bc2c9a06
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2025-02-05 12:35:35 +0530

    api error (405 method not allowed)

--- DevTinder-frontend/src/constants/routes.js
+++ DevTinder-frontend/src/constants/routes.js
@@ -1,10 +1,12 @@
-export const LOGIN_URL = "https://devtinder-app-dun.vercel.app/api/login"
-export const SIGNUP_URL = "https://devtinder-app-dun.vercel.app/api/signup"
-export const VIEW_PROFILE = "https://devtinder-app-dun.vercel.app/api/profile"
-export const LOGOUT_URL = "https://devtinder-app-dun.vercel.app/api/logout"
-export const FEED_URL = "https://devtinder-app-dun.vercel.app/api/user/feed"
-export const EDIT_PROFILE_URL = "https://devtinder-app-dun.vercel.app/api/profile/edit"
-export const RECEIVED_CONNECTION_REQUEST_URL = "https://devtinder-app-dun.vercel.app/api/user/request/received"
-export const REVIEW_CONNECTION_REQUEST_URL = "https://devtinder-app-dun.vercel.app/api/request/review"
-export const GET_CONNECTIONS_URL = "https://devtinder-app-dun.vercel.app/api/user/connections"
-export const SEND_CONNECTION_URL = "https://devtinder-app-dun.vercel.app/api/request/send"
+const BASE_URL = "https://devtinder-app-dun.vercel.app/api";
+
+export const LOGIN_URL = `${BASE_URL}/login`;
+export const SIGNUP_URL = `${BASE_URL}/signup`;
+export const VIEW_PROFILE = `${BASE_URL}/profile`;
+export const LOGOUT_URL = `${BASE_URL}/logout`;
+export const FEED_URL = `${BASE_URL}/user/feed`;
+export const EDIT_PROFILE_URL = `${BASE_URL}/profile/edit`;
+export const RECEIVED_CONNECTION_REQUEST_URL = `${BASE_URL}/user/request/received`;
+export const REVIEW_CONNECTION_REQUEST_URL = `${BASE_URL}/request/review`;
+export const GET_CONNECTIONS_URL = `${BASE_URL}/user/connections`;
+export const SEND_CONNECTION_URL = `${BASE_URL}/request/send`;
